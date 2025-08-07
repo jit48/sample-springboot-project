741d18c813e7f076e42d65d670e95bfab24098d5
JitGhosh28 <jitghosh2803@gmail.com>
2025-08-07 10:48:57 +0530

    gpt commit for CPG-7

--- src/main/java/com/sample/springboot/UserController.java
+++ src/main/java/com/sample/springboot/UserController.java
@@ -1,9 +1,11 @@
+```java
 package com.sample.springboot;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
@@ -22,10 +24,19 @@ public class UserController {
         return userRepository.findAll();
     }
 
-    // Bug: It deletes instead of updating
     @PutMapping("/{id}")
-    public void updateUser(@PathVariable Long id, @RequestBody User user) {
-        userRepository.deleteById(id);
+    public User updateUser(@PathVariable Long id, @RequestBody User user) {
+        Optional<User> existingUser = userRepository.findById(id);
+        if (existingUser.isPresent()) {
+            User updatedUser = existingUser.get();
+            updatedUser.setName(user.getName()); // assuming User has a setName method
+            updatedUser.setEmail(user.getEmail()); // assuming User has a setEmail method
+            // add other fields to be updated as necessary
+            return userRepository.save(updatedUser);
+        } else {
+            // handle case where user does not exist
+            throw new ResourceNotFoundException("User not found with id " + id);
+        }
     }
 }
-
+```
